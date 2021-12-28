eefe210c4fd417d9b140fb4d3b0d5274c75f541f
Utshab Roy <utshab.roy@gmail.com>
2021-12-28 12:18:48 +0900

    delete function impl with path variable

--- src/main/java/com/utshab/demo/student/StudentController.java
+++ src/main/java/com/utshab/demo/student/StudentController.java
@@ -27,4 +27,10 @@ public class StudentController {
     public void registerNewStudent(@RequestBody Student student){
         studentService.addNewStudent(student);
     }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId){
+        studentService.deleteStudent(studentId);
+
+    }
 }
--- src/main/java/com/utshab/demo/student/StudentService.java
+++ src/main/java/com/utshab/demo/student/StudentService.java
@@ -3,29 +3,36 @@ package com.utshab.demo.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class StudentService {
     private final StudentRepository studentRepository;
+
     @Autowired
     public StudentService(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
     }
 
-    public List<Student> getStudents(){
+    public List<Student> getStudents() {
         return studentRepository.findAll();
     }
 
     public void addNewStudent(Student student) {
         Optional<Student> studentByEmail = studentRepository.findStudentByEmail(student.getEmail());
-        if (studentByEmail.isPresent()){
+        if (studentByEmail.isPresent()) {
             throw new IllegalStateException("Email is taken !");
         }
         // the email is not taken so save the student
         studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if (!exists) {
+            throw new IllegalStateException("Student does not exists: " + studentId);
+        }
+        studentRepository.deleteById(studentId);
     }
 }
