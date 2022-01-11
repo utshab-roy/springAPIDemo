02f1a1010c5cf0d5a63c4e5897c6786e61243075
Utshab Roy <utshab.roy@gmail.com>
2022-01-11 19:13:43 +0900

    Student repository and one test class created for testing the layer

---
+++ src/test/java/com/utshab/demo/repository/StudentRepositoryTest.java
@@ -0,0 +1,28 @@
+package com.utshab.demo.repository;
+
+import com.utshab.demo.entity.Student;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class StudentRepositoryTest {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Test
+    public void saveStudent() {
+        Student student = Student.builder()
+                .emailId("shabbir@gmail.com")
+                .firstName("Shabbir")
+                .lastName("Dawoodi")
+                .guardianName("Nikhil")
+                .guardianEmail("nikhil@gmail.com")
+                .guardianMobile("9999999999")
+                .build();
+
+        studentRepository.save(student);
+    }
+}
