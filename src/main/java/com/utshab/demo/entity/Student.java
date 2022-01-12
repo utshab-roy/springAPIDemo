57030166b30f15dda922004d1e1b8b0175b39048
Utshab Roy <utshab.roy@gmail.com>
2022-01-12 17:56:06 +0900

    Guardian embedded class created for student and basic save test created with embedded func

--- src/main/java/com/utshab/demo/entity/Student.java
+++ src/main/java/com/utshab/demo/entity/Student.java
@@ -43,7 +43,7 @@ public class Student {
             nullable = false
     )
     private String emailId;
-    private String guardianName;
-    private String guardianEmail;
-    private String guardianMobile;
+
+    @Embedded
+    private Guardian guardian;
 }
--- src/test/java/com/utshab/demo/repository/StudentRepositoryTest.java
+++ src/test/java/com/utshab/demo/repository/StudentRepositoryTest.java
@@ -1,9 +1,12 @@
 package com.utshab.demo.repository;
 
+import com.utshab.demo.entity.Guardian;
 import com.utshab.demo.entity.Student;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 
 @SpringBootTest
@@ -18,11 +21,37 @@ class StudentRepositoryTest {
                 .emailId("shabbir@gmail.com")
                 .firstName("Shabbir")
                 .lastName("Dawoodi")
-                .guardianName("Nikhil")
-                .guardianEmail("nikhil@gmail.com")
-                .guardianMobile("9999999999")
+//                .guardianName("Nikhil")
+//                .guardianEmail("nikhil@gmail.com")
+//                .guardianMobile("9999999999")
                 .build();
 
         studentRepository.save(student);
+    }
+
+    @Test
+    public void saveStudentWithGuardian(){
+
+        Guardian guardian = Guardian.builder()
+                .email("atik@gmail.com")
+                .name("Atiquer")
+                .mobile("1234111")
+                .build();
+
+        Student student = Student.builder()
+                .firstName("Ashik")
+                .lastName("Jholok")
+                .emailId("ashik@gmail.com")
+                .guardian(guardian)
+                .build();
+
+        studentRepository.save(student);
+    }
+
+    @Test
+    public void printAllStudents(){
+        List<Student> studentList = studentRepository.findAll();
+
+        System.out.println(studentList);
     }
 }
