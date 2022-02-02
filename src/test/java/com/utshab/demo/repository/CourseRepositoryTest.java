8e6429af6e0e1f31a46d06558995cf438663c956
Utshab Roy <utshab.roy@gmail.com>
2022-02-02 19:21:38 +0900

    example of paging of data, and different type of available method

--- src/test/java/com/utshab/demo/repository/CourseRepositoryTest.java
+++ src/test/java/com/utshab/demo/repository/CourseRepositoryTest.java
@@ -5,6 +5,8 @@ import com.utshab.demo.entity.Teacher;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -44,5 +46,26 @@ class CourseRepositoryTest {
                 .build();
 
         courseRepository.save(course);
+    }
+
+    @Test
+    public void findAllPagination(){
+        // example of paging the data
+        // first parameter is page, it will divide the whole data divide by the size
+        // and size is the number of row we want to retrieve
+        Pageable pageWithTwoRecords = PageRequest.of(0, 2); // get course id 1, 2
+        Pageable pageWithThreeRecords = PageRequest.of(1, 3); // get course id 4, 5, 6
+
+        List<Course> courses = courseRepository.findAll(pageWithTwoRecords).getContent();
+        List<Course> secondPageCourses = courseRepository.findAll(pageWithThreeRecords).getContent();
+
+        System.out.println("First page:" + courses);
+        System.out.println("Second page:" + secondPageCourses);
+
+        long totalPages = courseRepository.findAll(pageWithTwoRecords).getTotalPages();
+        System.out.println("Total number of pages: " + totalPages);
+
+        long allElements = courseRepository.findAll(pageWithTwoRecords).getTotalElements();
+        System.out.println("Total number of elements: " + allElements);
     }
 }
